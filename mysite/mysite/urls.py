1dd19e51aaa7e7a7163f7f5ecbac40ff8f18d795
EmanuelMartinez23 <emanuelmartinez230801@gmail.com>
2024-10-14 22:41:18 -0600

    change urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,17 +1,10 @@
-import os
-from django.contrib import admin
-from django.urls import include, path, re_path
-from django.views.static import serve
-
-# Up two folders to serve "site" content
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SITE_ROOT = os.path.join(BASE_DIR, 'site')
+from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),                                                                                           
-    re_path(r'^site/(?P<path>.*)$', serve,
-        {'document_root': SITE_ROOT, 'show_indexes': True},
-        name='site_path'
-    ),
+    path('polls/', include('polls.urls')),
+    path('', views.home, name='home'),  # Add this line for the root URL
+    path('site/<path:path>/', views.site_path, name='site_path'),
 ]
+
