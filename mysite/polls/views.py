5965bf76abb642d521aa30bde764e68489e985f6
EmanuelMartinez23 <emanuelmartinez230801@gmail.com>
2024-10-18 18:23:25 -0600

    Fix import object get_object_or_404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
+
+
+
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = { "latest_question_list" : latest_question_list }
