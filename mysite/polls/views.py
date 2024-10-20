b84c9db7e9c5980c3a5523bbb2f04c4ce8bfe3e4
EmanuelMartinez23 <emanuelmartinez230801@gmail.com>
2024-10-20 17:30:47 -0600

    Small changes selected choice

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -23,7 +23,7 @@ def vote(request, question_id):
 
     question = get_object_or_404(Question, pk=question_id)
     try:
-        select_choice = question.choice_set.get(pk=request.POST["choice"])
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form
         return render(
