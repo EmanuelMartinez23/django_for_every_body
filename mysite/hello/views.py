ba7ee4c0d3ab352d7fcfe298d8aefdd38cdf38c2
EmanuelMartinez23 <emanuelmartinez230801@gmail.com>
2024-11-02 20:21:32 -0600

    Add cookie in  view

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -24,4 +24,5 @@ def sessfun(request) :
     request.session['num_visits'] = num_visits 
     if num_visits > 4 : del(request.session['num_visits'])
     resp = HttpResponse('view count='+str(num_visits))
+    resp.set_cookie('dj4e_cookie', '172975a7', max_age =1000 )
     return resp
