7470cb767059270d567f9c83a7a447681b83f420
Emanuel <emanuelmartinez230801@gmail.com>
2025-03-15 11:47:11 -0600

    Cats CRUD

--- mysite/cats/views.py
+++ mysite/cats/views.py
@@ -6,6 +6,8 @@ from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 from autos.models import Auto, Make
 from autos.forms import MakeForm
+from cats.models import Cat, Breed
+
 
 class CatCreate(LoginRequiredMixin, CreateView):
     model = Cat
@@ -19,8 +21,36 @@ class CatUpdate(LoginRequiredMixin, UpdateView):
     success_url = reverse_lazy('cats:all')
 
 
-class CatList(LoginRequiredMixin, ListView):
+class CatDelete(LoginRequiredMixin, DeleteView):
     model = Cat
     fields = '__all__'
     success_url = reverse_lazy('cats:all')
 
+class BreedList(LoginRequiredMixin, ListView):
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+class BreedDelete(LoginRequiredMixin, DeleteView):
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+class BreedUpdate(LoginRequiredMixin, UpdateView):
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+class BreedCreate(LoginRequiredMixin, CreateView):
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class MainView(LoginRequiredMixin, View):
+    def get(self, request):
+        bd = Breed.objects.count()
+        cats = Cat.objects.all()
+
+        ctx = {'breed_count': bd, 'cat_list': cats}
+        return render(request, 'cats/cat_list.html', ctx)
