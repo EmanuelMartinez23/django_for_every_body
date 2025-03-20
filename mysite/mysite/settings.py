bd6f8186f88d840bdbde1c4cfba64fc0238a6bba
Emanuel <emanuelmartinez230801@gmail.com>
2025-03-19 18:39:53 -0600

    Configuration PythonAnywhere

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,13 +119,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'emanuel13$ads',
+        'USER': 'emanuel13',
+        'PASSWORD': 'Pharsa13',
+        'HOST': 'emanuel13.mysql.pythonanywhere-services.com',
+         'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
