980ffa7c62ffe3d68549563ccbb02c770aa97fb1
Emanuel <emanuelmartinez230801@gmail.com>
2025-03-19 21:05:28 -0600

    Duumb

--- mysite/ads/models.py
+++ mysite/ads/models.py
@@ -7,11 +7,12 @@ from django.db import models
 class Ad(models.Model):
     title = models.CharField(
         max_length=200,
-        validators= [ MinLengthValidator(2, "Title must be greater than 2 characters")]
+        validators= [ MinLengthValidator(2, "Title must be greater than 2 characters")],
+        verbose_name="title"
     )
     # price = models.DecimalField(max_digits= 7, decimal_places = 2, null = True)
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True, verbose_name="precio")
-    text = models.TextField()
+    text = models.TextField(verbose_name="text")
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete =  models.CASCADE)
     created_at  = models.DateTimeField(auto_now_add  =True)
     updated_at =  models.DateTimeField(auto_now = True)
